a7a2733f83e172bdd02673f321dc28a79cd1ec26
Amir Mohammad <amirfanweb@gmail.com>
2024-07-14 01:20:36 +0330

    git add .

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import WebApp from "@twa-dev/sdk";
 function App() {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log(WebApp.initData);
+  }, []);
 
   return (
     <>
